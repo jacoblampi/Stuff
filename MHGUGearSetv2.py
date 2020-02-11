33a2fe614e093868e6f5fcdb72ca9544b7b36848
jacoblampi <jacoblampi@gmail.com>
2020-02-11 00:47:51 -0600

    proposed gem slot tracking in gear set

--- MHGUGearSetv2.py
+++ MHGUGearSetv2.py
@@ -1,13 +1,14 @@
 # Build the program like you'd use a visual interface
 
-# Gear exists as individual pieces 
-# Beginning with no gems, 
+# Gear exists as individual pieces
 class Gear():
     def __init__(self, name='', piece='', skills={}, slots=0, rankreq=0, huntertype=''):
         self.name = name 
         self.piece = piece 
-        self.skills =  skills 
-        self.slots = slots
+        self.skills = skills
+        self.slotsmax = slots
+        self.slotsopen = slots
+        self.slotsused = 0
         self.rankreq = rankreq 
         self.huntertype = huntertype
 
@@ -15,9 +16,8 @@ class Gear():
 class GearSet():
     def __init__(self, head=Gear(), body=Gear(), arms=Gear(), legs=Gear(), feet=Gear()):
         self.gs = {'Head':head, 'Body':body, 'Arms':arms, 'Legs':legs, 'Feet':feet}
-        self.gsslots = {'Head':self.gs['Head'].slots, 'Body':self.gs['Body'].slots,'Arms':self.gs['Arms'].slots,'Legs':self.gs['Legs'].slots,'Feet':self.gs['Feet'].slots}
+        self.gsslots = self.updateslots()
         self.gsskills = self.setskills()
-        
 
     def addgear(self, gobj):
         self.gs[gobj.piece] = gobj 
@@ -26,9 +26,12 @@ class GearSet():
     def removegear(self, piece):
         self.gs[piece] = Gear()
         self.gsskills = self.setskills()
-    
-    # def replacegear(self, gobj):
-    #     self.gs[gobj.piece] = gobj
+
+    def updateslots(self):
+        slots = {}
+        for key in self.gs:
+            slots[self.gs[key].piece] = [self.gs[key].slotsmax, self.gs[key].slotsused, self.gs[key].slotsopen]
+        return slots
 
     def setskills(self):
         gsskills = {}
@@ -57,18 +60,8 @@ gearlist.append(Gear('Narg Legs','Legs',{},3,4,'Blademaster'))
 gearlist.append(Gear('Narg Feet','Feet',{'Evade Dist':4},1,4,'Blademaster'))
 gearlist.append(Gear('Cean Body','Body',{'Sharpness':2,'Expert':2},0,4,'Blademaster'))
 
-testgs = GearSet()
-# testgs.addgear(gearlist[0])
-testgs.addgear(gearlist[1])
-testgs.addgear(gearlist[2])
-testgs.addgear(gearlist[3])
-testgs.addgear(gearlist[4])
-
-# print testgs.gsskills
-
 testgs2 = GearSet(gearlist[0], gearlist[1], gearlist[2], gearlist[3], gearlist[4])
 
 print testgs2.gsskills
 print testgs2.gsslots
-# for key in testgs.gs:
-#     print testgs.gs[key].skills
+
