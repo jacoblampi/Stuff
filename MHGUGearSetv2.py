8b9a52ce6e27c50be1798316093a06e5c65ad978
jacoblampi <56094956+jacoblampi@users.noreply.github.com>
2020-02-11 16:27:11 -0600

    Update MHGUGearSetv2.py
    Preliminary Gem Functionality

--- MHGUGearSetv2.py
+++ MHGUGearSetv2.py
@@ -3,22 +3,58 @@
 # Gear exists as individual pieces 
 # Beginning with no gems, 
 class Gear():
-    def __init__(self, name='', piece='', skills={}, slots=0, rankreq=0, huntertype=''):
+    def __init__(self, name='', piece='', skills={}, slotsmax=0, rankreq=0, huntertype=''):
         self.name = name 
         self.piece = piece 
         self.skills =  skills 
-        self.slots = slots
+        self.slotsmax = slotsmax 
+        self.slotsused = 0
+        self.slotsopen = slotsmax
         self.rankreq = rankreq 
         self.huntertype = huntertype
+        self.gems = []
+
+    def addgem(self, gem):
+        self.gems.append(gem)
+        for key in gem.skills:
+            if key in self.skills:
+                self.skills[key] += gem.skills[key]
+            else:
+                self.skills[key] = gem.skills[key]
+        self.slotsused += gem.slotsreq 
+        self.slotsopen = self.slotsmax - self.slotsused
+    
+    def removegems(self):
+        for gem in self.gems:
+            for key in gem.skills:
+                self.skills[key] -= gem.skills[key]
+        newskilldict = {}
+        for key in self.skills:
+            if self.skills[key] != 0:
+                newskilldict[key] = self.skills[key]
+            else:
+                pass
+        self.skills = newskilldict
+        self.gems = []
+        self.slotsused = 0
+        self.slotsopen = self.slotsmax
+
+
+class Gem():
+    def __init__(self, name='', skills={}, slotsreq=0, rankreq=0):
+        self.name = name 
+        self.skills = skills 
+        self.slotsreq = slotsreq 
+        self.rankreq = rankreq
 
 
 class GearSet():
     def __init__(self, head=Gear(), body=Gear(), arms=Gear(), legs=Gear(), feet=Gear()):
         self.gs = {'Head':head, 'Body':body, 'Arms':arms, 'Legs':legs, 'Feet':feet}
-        self.gsslots = {'Head':self.gs['Head'].slots, 'Body':self.gs['Body'].slots,'Arms':self.gs['Arms'].slots,'Legs':self.gs['Legs'].slots,'Feet':self.gs['Feet'].slots}
+        self.gsslots = self.determineslots()
+        self.gsslotsopen = self.determineopenslots()
         self.gsskills = self.setskills()
         
-
     def addgear(self, gobj):
         self.gs[gobj.piece] = gobj 
         self.gsskills = self.setskills()
@@ -27,8 +63,17 @@ class GearSet():
         self.gs[piece] = Gear()
         self.gsskills = self.setskills()
     
-    # def replacegear(self, gobj):
-    #     self.gs[gobj.piece] = gobj
+    def determineslots(self):
+        gsslots = {}
+        for gkey in self.gs:
+            gsslots[gkey] = [self.gs[gkey].slotsused, self.gs[gkey].slotsopen, self.gs[gkey].slotsmax]
+        return gsslots
+
+    def determineopenslots(self):
+        openslots = {0:0, 1:0, 2:0, 3:0}
+        for key in self.gsslots:
+            openslots[self.gsslots[key][1]] += 1
+        return openslots
 
     def setskills(self):
         gsskills = {}
@@ -39,6 +84,16 @@ class GearSet():
                 else:
                     gsskills[skill] = self.gs[gkey].skills[skill]
         return gsskills
+
+    def addgem(self, gem, piecekey):
+        self.gs[piecekey].addgem(gem)
+        self.gsskills = self.setskills()
+    
+    def removegems(self, piecekey):
+        self.gs[piecekey].removegems()
+        self.gsskills = self.setskills()
+
+
 
 
 
@@ -57,18 +112,24 @@ gearlist.append(Gear('Narg Legs','Legs',{},3,4,'Blademaster'))
 gearlist.append(Gear('Narg Feet','Feet',{'Evade Dist':4},1,4,'Blademaster'))
 gearlist.append(Gear('Cean Body','Body',{'Sharpness':2,'Expert':2},0,4,'Blademaster'))
 
-testgs = GearSet()
-# testgs.addgear(gearlist[0])
-testgs.addgear(gearlist[1])
-testgs.addgear(gearlist[2])
-testgs.addgear(gearlist[3])
-testgs.addgear(gearlist[4])
-
-# print testgs.gsskills
-
 testgs2 = GearSet(gearlist[0], gearlist[1], gearlist[2], gearlist[3], gearlist[4])
 
 print testgs2.gsskills
-print testgs2.gsslots
+# print testgs2.gsslots
+# print testgs2.gsslotsopen
 # for key in testgs.gs:
 #     print testgs.gs[key].skills
+
+gemlist = [] 
+#Tier 1
+gemlist.append(Gem('Sharpness 1',{'Sharpness':1},1,2))
+gemlist.append(Gem('Expert 1',{'Expert':1},1,2))
+
+testgear = gearlist[0]
+testgem = gemlist[0]
+
+testgs2.addgem(testgem, 'Head')
+print testgs2.gsskills
+
+testgs2.removegems('Head')
+print testgs2.gsskills
