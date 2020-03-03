cde132411c3e8faa1a333cd95d382b0be73c51c5
jacoblampi <56094956+jacoblampi@users.noreply.github.com>
2020-03-03 16:30:54 -0600

    Create Tracking.py

---
+++ Tracking.py
@@ -0,0 +1,36 @@
+
+
+class Project():
+    
+
+#Study Area
+#Project "Group"
+#WO Number
+#WO Name
+#Year
+#EI Sketch 
+#Notes
+
+
+
+
+
+
+
+
+# import json
+# y = 30
+# x = {
+#   "name": "John",
+#   "age": 30,
+#   "married": True,
+#   "divorced": False,
+#   "children": ("Ann","Billy"),
+#   "pets": None,
+#   "cars": [
+#     {"model": "BMW 230", "mpg": 27.5},
+#     {"model": "Ford Edge", "mpg": 24.1}
+#   ]
+# }
+
+# print(json.dumps(x))
